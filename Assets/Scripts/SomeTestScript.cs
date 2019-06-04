6f131a6504744a69db8bd5212d0e02eaf8020544
Tetsuya Mori <monry84@gmail.com>
2019-06-04 11:17:37 +0900

    :baby: Initial commit

---
+++ Assets/Scripts/SomeTestScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace BugReports.TestProject
+{
+    public class SomeTestScript
+    {
+        public static void Foo()
+        {
+            Debug.Log("Bar");
+        }
+    }
+}
+
